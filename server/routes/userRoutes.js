07439a8c878c8aa0de798eada713f48f407c2d4d
Eliana Maricel Dominguez <102763298+lyrsk@users.noreply.github.com>
2023-05-24 19:40:27 -0300

    Delete server directory

--- server/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { register, login, changePassword, getAllUsers } from '../controllers/userController.js'
-
-export const router = express.Router()
-router.post('/register', register)
-router.post('/login', login)
-router.post('/password', changePassword)
-router.get('/allusers/:id', getAllUsers)
