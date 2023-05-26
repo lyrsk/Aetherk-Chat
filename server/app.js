d179d653f70ba8b7ceb04362249759559456bb65
Eliana <crisely.cye@gmail.com>
2023-05-26 01:29:45 -0300

    Rutas

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
-import dotenv from 'dotenv'; import mongoose from 'mongoose'
-dotenv.config()
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'; dotenv.config()
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -11,10 +12,10 @@ app.use(express.json())
 mongoose.connect(process.env.MONGODB_URI,
   {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-  }
-)
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err))
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`)
